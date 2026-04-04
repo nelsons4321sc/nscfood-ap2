3db5c95df5215271af656362bd170377f0d0b133
Nelson Souto Costa <nelsonscosta@yahoo.com.br>
2026-04-04 15:34:13 -0300

    Adicionando bean Notificador

--- src/main/java/com/nscworks/nscfoods/noticacao/Notificador.java
+++ src/main/java/com/nscworks/nscfoods/noticacao/Notificador.java
@@ -1,6 +1,5 @@
 package com.nscworks.nscfoods.noticacao;
 
-
 import org.springframework.stereotype.Component;
 
 import com.nscworks.nscfoods.modelo.Cliente;
@@ -9,45 +8,14 @@ import com.nscworks.nscfoods.modelo.Cliente;
 public class Notificador {
 	
 	public Notificador() {
-		System.out.println("Chamando o construtor com bean gerenciável");
-
+		System.out.println("Chamando o construtor Notificador");
 	}
 	
+
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.printf("Notificando %s através do email %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
-	
-	// private boolean caixaAlta;
-	// private String hostServidorSmtp;
-	
-/*
-	public Notificador(Cliente cliente, String mensagem) {
-		//this.hostServidorSmtp = hostServidoerSmtp;
-		System.out.printf("Notificando %s através do email %s: %s\n",
-				cliente.getNome(), cliente.getEmail(), mensagem);
-		
-		
-	}
-	
-	/*
-	@Override
-	public void notificar(Cliente cliente, String mensagem) {
-		
-		if(caixaAlta) {
-			mensagem = mensagem.toUpperCase();
-		}
-		
-		System.out.printf("Notificando %s através do e-mail %s usando o SMTP %s: %s\n", 
-				cliente.getNome(), cliente.getEmail(),this.hostServidorSmtp, mensagem);
-	}
-
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
-	}
-	
-	*/
-	
 
 }
