572da1ae2b1f91666dc7c3e252cb8be9d0e0a709
Nelson Souto Costa <nelsonscosta@yahoo.com.br>
2026-04-04 16:01:59 -0300

    injetando dependencias com bean

--- src/main/java/com/nscworks/nscfoods/service/AtivacaoClienteService.java
+++ src/main/java/com/nscworks/nscfoods/service/AtivacaoClienteService.java
@@ -1,12 +1,19 @@
 package com.nscworks.nscfoods.service;
 
+import org.springframework.stereotype.Component;
+
 import com.nscworks.nscfoods.modelo.Cliente;
 import com.nscworks.nscfoods.noticacao.Notificador;
 
-
+@Component
 public class AtivacaoClienteService {
 	
 	private Notificador notificador;
+	
+	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+		System.out.println("Chamando o construtor AtivacaoClienteService "+ notificador);
+	}
 	
 
 	public void ativar(Cliente cliente) {
