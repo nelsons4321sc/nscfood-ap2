88428bae9544da520d6775e1d382152e290ea112
Nelson Souto Costa <nelsonscosta@yahoo.com.br>
2026-04-06 07:08:39 -0300

    2213 - Condição mensagem caixa alta

--- src/main/java/com/nscworks/nscfoods/noticacao/NotificadorEmail.java
+++ src/main/java/com/nscworks/nscfoods/noticacao/NotificadorEmail.java
@@ -7,6 +7,8 @@ import com.nscworks.nscfoods.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
+	private boolean caixaAlta;
+	
 	public NotificadorEmail() {
 		System.out.println("Chamando o construtor Notificador");
 	}
@@ -14,9 +16,18 @@ public class NotificadorEmail implements Notificador {
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		
+		if(caixaAlta) {
+			mensagem = mensagem.toUpperCase();
+		}
+		
 		System.out.printf("Notificando %s através do email %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
+
+	public void setCaixaAlta(boolean caixaAlta) {
+		this.caixaAlta = caixaAlta;
+	}
 
 }
